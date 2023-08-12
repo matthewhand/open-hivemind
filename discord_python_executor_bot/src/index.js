3faa2cf34a76b3b7d14f2268ced95478852d7a5b
matthewhand <matthewhandau@gmail.com>
2023-08-12 21:13:22 +1000

    Add main.js

---
+++ discord_python_executor_bot/src/index.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.env.DISCORD_TOKEN;
+client.login(token);
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
