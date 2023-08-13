aa39cca37189ba6b9794b96784b2d8e8900e10d4
matthewhand <matthewhandau@gmail.com>
2023-08-13 16:02:17 +1000

    Merged index.js and main.js

---
+++ index.js
@@ -0,0 +1,39 @@
+
+const { Client, Intents } = require('discord.js');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === 'python') {
+    // Execute Python code here
+  }
+});
+
+app.post('/webhook', (req, res) => {
+  // Handle webhook here
+});
+
+client.login('YOUR_TOKEN_HERE');
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const token = process.env.DISCORD_TOKEN;
+client.login(token);
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
