c5438bfd3fef0916c4c891fc100abf24c95641ce
matthewhand <matthewhandau@gmail.com>
2023-08-13 18:40:25 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const token = process.env.DISCORD_TOKEN;
+const { Client, Intents } = require('discord.js');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand() || interaction.commandName !== 'python') return;
+
+  const pythonCode = interaction.options.getString('code');
+  if (!pythonCode) return;
+
+  exec(`python -c "${pythonCode}"`, (error, stdout, stderr) => {
+    if (error) {
+      interaction.reply(`Error executing Python code: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      interaction.reply(`Python Error: ${stderr}`);
+      return;
+    }
+    interaction.reply(`Python Output: ${stdout}`);
+  });
 });
 
-client.login(token);
+app.post('/webhook', (req, res) => {
+  // Handle webhook here
+});
+
+client.login(process.env.DISCORD_TOKEN);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
