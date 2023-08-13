4912e1fd5cdca00dbcfa13e127f9bb6d35368465
matthewhand <matthewhandau@gmail.com>
2023-08-13 19:49:52 +1000

    Create commands/python_command/python.js

---
+++ commands/python_command/python.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  data: {
+    name: 'python',
+    description: 'Execute Python code',
+  },
+  async execute(interaction) {
+    // Add logic to execute Python code
+    interaction.reply('Python command executed!');
+  },
+};
