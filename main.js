f94ab4a22c8ca27cc52af43332155952d18924fc
matthewhand <matthewhandau@gmail.com>
2023-08-13 16:02:17 +1000

    Merging index.js and main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const token = process.env.DISCORD_TOKEN;
-client.login(token);
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
