4bef6409ea11d455735ffdaadfcdfe5d4911945d
matthewhand <matthewhandau@gmail.com>
2023-08-13 19:54:44 +1000

    Update python.js

--- commands/python_command/python.js
+++ commands/python_command/python.js
@@ -1,3 +1,4 @@
+const { exec } = require('child_process');
 
 module.exports = {
   data: {
@@ -5,7 +6,16 @@ module.exports = {
     description: 'Execute Python code',
   },
   async execute(interaction) {
-    // Add logic to execute Python code
-    interaction.reply('Python command executed!');
+    const code = interaction.options.getString('code'); // Assuming the code is passed as a string option
+
+    // Sanitize the code if necessary
+
+    exec(`python -c "${code}"`, (error, stdout, stderr) => {
+      if (error) {
+        interaction.reply(`Error executing Python code:\nExit Code: ${error.code}\nStderr: ${stderr}\nStdout: ${stdout}`);
+        return;
+      }
+      interaction.reply(`Execution Successful:\nExit Code: 0\nStdout: ${stdout}\nStderr: ${stderr}`);
+    });
   },
 };
