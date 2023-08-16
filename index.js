997445bba69b9094e262292ea8de7c336cde570a
matthewhand <matthewhandau@gmail.com>
2023-08-16 20:34:37 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { Routes } = require('@discordjs/rest');
 const { Client, Intents } = require('discord.js');
 const { REST } = require('@discordjs/rest');
 const { exec } = require('child_process');
