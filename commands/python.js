cfd8620559cc9a9e2aa103e626e0249af8f7a334
matthewhand <matthewhandau@gmail.com>
2023-08-23 18:43:28 +1000

    Update python.js

--- commands/python.js
+++ commands/python.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const { exec } = require('child_process');
 const allowedUsers = process.env.ALLOWED_USERS.split(',');
 
