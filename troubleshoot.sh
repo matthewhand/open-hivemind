34db5764ff7ce711cd5e76365f7b13be6c0288cc
matthewhand <matthewhandau@gmail.com>
2023-08-23 10:42:29 +1000

    Update troubleshoot.sh

--- troubleshoot.sh
+++ troubleshoot.sh
@@ -1,14 +1,13 @@
 
 #!/bin/bash
-echo 'Running Troubleshoot Script'
-if [ -z "$ALLOWED_USERS" ]; then
-  echo 'ERROR: ALLOWED_USERS environment variable is not set.'
-  exit 1
-fi
-echo 'Environment variable ALLOWED_USERS is set.'
-echo 'Node.js version:'
+echo 'Running Troubleshooting Script...'
+echo 'Node Version:'
 node --version
-echo 'NPM version:'
+echo 'NPM Version:'
 npm --version
-echo 'Current directory content:'
-ls -la
+echo 'Current Directory:'
+pwd
+echo 'Listing Files:'
+ls -al
+echo 'Environment Variables:'
+env
