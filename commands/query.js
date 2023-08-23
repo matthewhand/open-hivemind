873af5511e37e71341eba8a9c1ec10295140df63
matthewhand <matthewhandau@gmail.com>
2023-08-23 18:21:47 +1000

    Create query.js

---
+++ commands/query.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('query')
+		.setDescription('Query a database via HTTP')
+		.addStringOption(option =>
+			option.setName('querystring')
+				.setDescription('The query string to send')
+				.setRequired(true)
+		),
+	async execute(interaction) {
+		const queryUrl = process.env.QUERY_URL;
+		const queryString = interaction.options.getString('querystring');
+		const encodedQueryString = encodeURIComponent(queryString);
+		const fullUrl = `${queryUrl}?query=${encodedQueryString}`;
+
+		axios.get(fullUrl)
+			.then((response) => {
+				// Check if the response data is JSON
+				if (typeof response.data === 'object' && response.data !== null) {
+					// Return the value for the key 'answer' if it exists
+					const answer = response.data.answer;
+					if (answer) {
+						interaction.reply(`Query result: ${answer}`);
+					} else {
+						interaction.reply(`Query result does not contain the key 'answer'`);
+					}
+				} else {
+					interaction.reply(`Query result: ${response.data}`);
+				}
+			})
+			.catch((error) => {
+				interaction.reply(`Error querying database: ${error.message}`);
+			});
+	},
+};
