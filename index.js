fab52ae631b5f8af9604a664d3e50742533afb7c
matthewhand <matthewhandau@gmail.com>
2023-08-23 18:13:04 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+//const { clientId, guildId, token } = require('./config.json');
+const clientId = process.env.CLIENT_ID;
+const token = process.env.DISCORD_TOKEN;
+const guildId = process.env.GUILD_ID;
+
 const fs = require('node:fs');
 const path = require('node:path');
 
