288f88331e5e0bd498c038aa28f365975d319450
matthewhand <matthewhandau@gmail.com>
2023-08-23 21:00:16 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const { exec } = require('child_process');
 const { registerCommands, handleCommands, commandExecutors } = require('./commands');
 const { startWebhookServer } = require('./webhook');
 
 const clientId = process.env.CLIENT_ID;
 const token = process.env.DISCORD_TOKEN;
 const guildId = process.env.GUILD_ID;
+const allowedUsers = process.env.ALLOWED_USERS.split(',');
 
 // Register Discord commands
 registerCommands(clientId, token, guildId);
 
 // Handle Discord commands
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent] });
 handleCommands(client);
+
+client.on('messageCreate', async (message) => {
+  // Check if the message is from a guild and contains a Python code block
+  if (message.guild && message.content.includes('```python')) {
+    const userId = message.author.id;
+    const guild = message.guild;
+    const member = message.member;
+
+    // Check if the member is the owner of the guild or an allowed user
+    if ((guild.ownerId !== member.id) && (!allowedUsers.includes(userId))) {
+      return message.reply('You do not have permission to execute this command.');
+    }
+
+    // Extract the Python code from the code block
+    const codeMatch = message.content.match(/```python\n([\s\S]+?)```/);
+    if (!codeMatch) return;
+
+    const code = codeMatch[1];
+    const escapedCode = code.replace(/"/g, '\\"'); // Escape double quotes
+
+    console.log(`Executing Python code: ${escapedCode}`); // Print the code argument
+
+    // Execute the Python code
+    exec(`python -c "${escapedCode}"`, (error, stdout, stderr) => {
+      if (error) {
+        message.reply(`Error executing code: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        message.reply(`Stderr: ${stderr}`);
+        return;
+      }
+      message.reply(`Stdout: ${stdout}`);
+    });
+  }
+});
+
+
 client.login(token);
 
 // Start webhook server
