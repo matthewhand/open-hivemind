3c74012adfd34e1776d1bbe3d1e3248e6189e10d
matthewhand <matthewhandau@gmail.com>
2023-08-24 06:19:16 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const { exec } = require('child_process');
 const { registerCommands, handleCommands, commandExecutors } = require('./commands');
 const { startWebhookServer } = require('./webhook');
+const winston = require('winston');
 
 const clientId = process.env.CLIENT_ID;
 const token = process.env.DISCORD_TOKEN;
 const guildId = process.env.GUILD_ID;
 const allowedUsers = process.env.ALLOWED_USERS.split(',');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({ format: winston.format.simple() }),
+  ],
+});
 
 // Register Discord commands
 registerCommands(clientId, token, guildId);
@@ -15,53 +24,52 @@ registerCommands(clientId, token, guildId);
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent] });
 handleCommands(client);
 client.on('messageCreate', async (message) => {
-  console.log('Message received:', message.content); // Log the received message
+  try {
+    logger.info('Message received:', message.content);
 
-  // Check if the message is from a guild and contains 'pybot'
-  if (message.guild && message.content.toLowerCase().includes('pybot')) {
-    console.log('Message contains "pybot"'); // Log that the message contains "pybot"
+    if (message.guild && message.content.toLowerCase().includes('pybot')) {
+      logger.info('Message contains "pybot"');
 
-    const userId = message.author.id;
-    const guild = message.guild;
-    const member = message.member;
+      const userId = message.author.id;
+      const guild = message.guild;
+      const member = message.member;
 
-    // Check if the member is the owner of the guild or an allowed user
-    if ((guild.ownerId !== member.id) && (!allowedUsers.includes(userId))) {
-      console.log('User does not have permission'); // Log permission check
-      return message.reply('You do not have permission to execute this command.');
-    }
+      if ((guild.ownerId !== member.id) && (!allowedUsers.includes(userId))) {
+        logger.warn('User does not have permission');
+        return message.reply('You do not have permission to execute this command.');
+      }
 
-    // Extract all Python code blocks from the message
-    const codeBlocks = message.content.match(/\`\`\`python\n?([\s\S]+?)\`\`\`/g);
-    if (!codeBlocks) {
-      console.log('No Python code blocks found'); // Log if no code blocks are found
-      return;
-    }
+      const codeBlocks = message.content.match(/\`\`\`python\n?([\s\S]+?)\`\`\`/g);
+      if (!codeBlocks) {
+        logger.info('No Python code blocks found');
+        return;
+      }
 
-    console.log('Found Python code blocks:', codeBlocks); // Log the found code blocks
+      logger.info('Found Python code blocks:', codeBlocks);
 
-    codeBlocks.forEach((codeBlock) => {
-      // Extract the Python code from the code block
-      const code = codeBlock.replace(/\`\`\`python\n?|\`\`\`/g, '');
-      const escapedCode = code.replace(/"/g, '\\"'); // Escape double quotes
+      codeBlocks.forEach((codeBlock) => {
+        const code = codeBlock.replace(/\`\`\`python\n?|\`\`\`/g, '');
+        const escapedCode = code.replace(/"/g, '\\"');
 
-      console.log(`Executing Python code: ${escapedCode}`); // Print the code argument
+        logger.info(`Executing Python code: ${escapedCode}`);
 
-      // Execute the Python code
-      exec(`python -c "${escapedCode}"`, (error, stdout, stderr) => {
-        if (error) {
-          message.reply(`Error executing code: ${error.message}`);
-          return;
-        }
-        if (stderr) {
-          message.reply(`Stderr: ${stderr}`);
-          return;
-        }
-        message.reply(`Stdout: ${stdout}`);
+        exec(`python -c "${escapedCode}"`, (error, stdout, stderr) => {
+          if (error) {
+            message.reply(`Error executing code: ${error.message}`);
+            return;
+          }
+          if (stderr) {
+            message.reply(`Stderr: ${stderr}`);
+            return;
+          }
+          message.reply(`Stdout: ${stdout}`);
+        });
       });
-    });
-  } else {
-    console.log('Message does not contain "pybot"'); // Log if the message does not contain "pybot"
+    } else {
+      logger.info('Message does not contain "pybot"');
+    }
+  } catch (error) {
+    logger.error('An error occurred:', error);
   }
 });
 
