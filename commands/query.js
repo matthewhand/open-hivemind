550bc26f59f9b585cc81b303605406c56c99c75a
matthewhand <matthewhandau@gmail.com>
2023-08-23 20:35:07 +1000

    Update query.js

--- commands/query.js
+++ commands/query.js
@@ -10,29 +10,31 @@ module.exports = {
 				.setDescription('The query string to send')
 				.setRequired(true)
 		),
-	async execute(interaction) {
-		const queryUrl = process.env.QUERY_URL;
-		const queryString = interaction.options.getString('querystring');
-		const encodedQueryString = encodeURIComponent(queryString);
-		const fullUrl = `${queryUrl}?query=${encodedQueryString}`;
+async execute(interaction) {
+	const queryUrl = process.env.QUERY_URL;
+	const queryString = interaction.options.getString('querystring');
+	const encodedQueryString = encodeURIComponent(queryString);
+	const fullUrl = `${queryUrl}?query=${encodedQueryString}`;
 
-		axios.get(fullUrl)
-			.then((response) => {
-				// Check if the response data is JSON
-				if (typeof response.data === 'object' && response.data !== null) {
-					// Return the value for the key 'answer' if it exists
-					const answer = response.data.answer;
-					if (answer) {
-						interaction.followUp(`Query result: ${answer}`);
-					} else {
-						interaction.followUp(`Query result does not contain the key 'answer'`);
-					}
+	await interaction.deferReply(); // Acknowledge the command immediately
+
+	axios.get(fullUrl)
+		.then((response) => {
+			// Check if the response data is JSON
+			if (typeof response.data === 'object' && response.data !== null) {
+				// Return the value for the key 'answer' if it exists
+				const answer = response.data.answer;
+				if (answer) {
+					interaction.followUp(`Query result: ${answer}`);
 				} else {
-					interaction.followUp(`Query result: ${response.data}`);
+					interaction.followUp(`Query result does not contain the key 'answer'`);
 				}
-			})
-			.catch((error) => {
-				interaction.followUp(`Error querying database: ${error.message}`);
-			});
-	},
+			} else {
+				interaction.followUp(`Query result: ${response.data}`);
+			}
+		})
+		.catch((error) => {
+			interaction.followUp(`Error querying database: ${error.message}`);
+		});
+},
 };
