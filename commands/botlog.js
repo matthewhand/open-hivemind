ab00c9f6117fdf035e8eebfdbbd89e6da3ddf862
matthewhand <matthewhandau@gmail.com>
2023-08-26 10:42:39 +1000

    Update botlog.js with logging functionality

--- commands/botlog.js
+++ commands/botlog.js
@@ -1,2 +1,24 @@
-// This is the botlog.js file
-console.log('Bot logs will be here.');
+
+const Discord = require('discord.js');
+const fs = require('fs');
+const winston = require('winston');
+
+module.exports = {
+    name: 'botlog',
+    description: 'Fetch and display bot logs.',
+    execute(message, args) {
+        // Read logs from the file
+        fs.readFile('./logs/bot.log', 'utf8', (err, data) => {
+            if (err) {
+                winston.error(`Error reading log file: ${err}`);
+                return message.channel.send('Error reading log file.');
+            }
+            // Send logs to Discord channel
+            const logEmbed = new Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('Bot Logs')
+                .setDescription(`\`\`\n${data}\`\`\`);
+            message.channel.send(logEmbed);
+        });
+    },
+};
