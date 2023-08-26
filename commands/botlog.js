7f0a61fa93a18a4c694c6e0bd8ea286d4d32e499
matthewhand <matthewhandau@gmail.com>
2023-08-26 11:31:21 +1000

    Update botlog.js

--- commands/botlog.js
+++ commands/botlog.js
@@ -1,29 +1,26 @@
-const Discord = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const fs = require('fs');
 const winston = require('winston');
 
 module.exports = {
-    name: 'botlog',
-    description: 'Fetch and display bot logs.',
-    execute(message, args) {
-        // Read logs from the file
+    data: new SlashCommandBuilder()
+        .setName('botlog')
+        .setDescription('Fetch and display bot logs.'),
+    async execute(interaction) {
+        await interaction.deferReply();
         fs.readFile('./logs/bot.log', 'utf8', (err, data) => {
             if (err) {
                 winston.error(`Error reading log file: ${err}`);
-                return message.channel.send('Error reading log file.');
+                return interaction.followUp('Error reading log file.');
             }
-            
-            // Check if data length exceeds Discord's limit
             if (data.length > 2048) {
                 data = data.substring(0, 2045) + '...';
             }
-
-            // Send logs to Discord channel
             const logEmbed = new Discord.MessageEmbed()
                 .setColor('#0099ff')
                 .setTitle('Bot Logs')
-                .setDescription('`\`\`${data}\`\`\`');
-            message.channel.send(logEmbed);
+                .setDescription(`\`\`\`${data}\`\`\``);
+            interaction.followUp({ embeds: [logEmbed] });
         });
     },
 };
