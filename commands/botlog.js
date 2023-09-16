eee96c4179212f951f57410a7001001499d020f4
matthewhand <matthewhandau@gmail.com>
2023-09-16 20:50:00 +1000

    Update botlog.js

--- commands/botlog.js
+++ commands/botlog.js
@@ -1,30 +1,35 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder } = require('discord.js');
 const fs = require('fs');
-const winston = require('winston');
-const { MessageEmbed } = require('discord.js');  // Changed from EmbedBuilder to MessageEmbed
+const path = require('path');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('botlog')
-        .setDescription('Fetch and display bot logs.'),
-    async execute(interaction) {
-        await interaction.deferReply();
-        fs.readFile('./logs/bot.log', 'utf8', (err, data) => {
-            if (err) {
-                winston.error(`Error reading log file: ${err}`);
-                return interaction.followUp('Error reading log file.');
-            }
-            
-            if (data.length > 2048) {
-                data = data.slice(-2045) + '...';  // Getting the last 2045 characters
-            }
-            
-            const logEmbed = new MessageEmbed()  // Changed from EmbedBuilder to MessageEmbed
-                .setColor('#0099ff')
-                .setTitle('Bot Logs')
-                .setDescription(`${data}`);
-                
-            interaction.followUp({ embeds: [logEmbed] });
-        });
-    },
+  name: 'botlog',
+  description: 'Fetches the bot log',
+  execute(interaction, client, Discord) {
+    const logsDir = path.join(__dirname, '../logs');
+    const logFile = path.join(logsDir, 'bot.log');
+
+    if (!fs.existsSync(logsDir)) {
+      fs.mkdirSync(logsDir);
+    }
+
+    fs.readFile(logFile, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return interaction.reply('An error occurred while reading the log file.');
+      }
+
+      if (data.length > 2048) {
+        data = data.slice(-2045) + '...';  // Getting the last 2045 characters
+      }
+
+
+      const logEmbed = new EmbedBuilder()
+        .setColor('#0099ff')
+        .setTitle('Bot Log')
+        .setDescription('```' + data.substring(0, 2048) + '```');
+
+      interaction.reply({ embeds: [logEmbed] });
+    });
+  },
 };
