2498b60f7b1ff336861ea9fc2f64006a608392f6
matthewhand <matthewhandau@gmail.com>
2023-09-16 20:34:18 +1000

    Update botlog.js

--- commands/botlog.js
+++ commands/botlog.js
@@ -2,7 +2,31 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const fs = require('fs');
 const readline = require('readline');
 const winston = require('winston');
-const { MessageEmbed } = require('discord.js'); // It should be MessageEmbed, not EmbedBuilder.
+const { MessageEmbed } = require('discord.js'); 
+
+const readLastLines = (path, numberOfLines) => {
+  return new Promise((resolve, reject) => {
+    const fileStream = fs.createReadStream(path);
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity
+    });
+
+    let lastLines = [];
+    rl.on('line', (line) => {
+      lastLines.push(line);
+      if (lastLines.length > numberOfLines) {
+        lastLines.shift();
+      }
+    });
+
+    rl.on('close', () => {
+      resolve(lastLines.reverse().join('\n'));
+    });
+
+    rl.on('error', reject);
+  });
+};
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,32 +34,22 @@ module.exports = {
         .setDescription('Fetch and display bot logs.'),
     async execute(interaction) {
         await interaction.deferReply();
-        
-        const fileStream = fs.createReadStream('./logs/bot.log');
-        const rl = readline.createInterface({
-            input: fileStream,
-            crlfDelay: Infinity
-        });
 
-        let lastLines = [];
-        for await (const line of rl) {
-            lastLines.push(line);
-            if (lastLines.length > 10) {
-                lastLines.shift();
-            }
-        }
-        
-        let data = lastLines.reverse().join('\n');
-        
-        if (data.length > 2048) {
+        try {
+          const data = await readLastLines('./logs/bot.log', 10);
+          if (data.length > 2048) {
             data = data.substring(0, 2045) + '...';
-        }
+          }
 
-        const logEmbed = new MessageEmbed()
+          const logEmbed = new MessageEmbed()
             .setColor('#0099ff')
             .setTitle('Bot Logs')
-            .setDescription(`${data}`);
-        
-        interaction.followUp({ embeds: [logEmbed] });
+            .setDescription(data);
+          
+          interaction.followUp({ embeds: [logEmbed] });
+        } catch (err) {
+          winston.error(`Error reading log file: ${err}`);
+          interaction.followUp('Error reading log file.');
+        }
     },
 };
