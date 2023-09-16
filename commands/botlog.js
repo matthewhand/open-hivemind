b47c43c17a74bb2f045b21ed8ac6900fe4d92771
matthewhand <matthewhandau@gmail.com>
2023-09-16 21:20:46 +1000

    Update botlog.js

--- commands/botlog.js
+++ commands/botlog.js
@@ -1,9 +1,4 @@
-const { EmbedBuilder } = require('discord.js');
-const fs = require('fs');
-const path = require('path');
-
-module.exports = {
-  name: 'botlog',const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 
@@ -38,36 +33,6 @@ module.exports = {
         .setDescription('```' + data.substring(0, 2048) + '```');
 
       interaction.followUp({ embeds: [logEmbed] });
-    });
-  },
-};
-
-  description: 'Fetches the bot log',
-  execute(interaction, client, Discord) {
-    const logsDir = path.join(__dirname, '../logs');
-    const logFile = path.join(logsDir, 'bot.log');
-
-    if (!fs.existsSync(logsDir)) {
-      fs.mkdirSync(logsDir);
-    }
-
-    fs.readFile(logFile, 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return interaction.reply('An error occurred while reading the log file.');
-      }
-
-      if (data.length > 2048) {
-        data = data.slice(-2045) + '...';  // Getting the last 2045 characters
-      }
-
-
-      const logEmbed = new EmbedBuilder()
-        .setColor('#0099ff')
-        .setTitle('Bot Log')
-        .setDescription('```' + data.substring(0, 2048) + '```');
-
-      interaction.reply({ embeds: [logEmbed] });
     });
   },
 };
