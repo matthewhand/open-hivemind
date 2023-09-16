80b869af27dda763071802753b411c987b0761d6
matthewhand <matthewhandau@gmail.com>
2023-09-16 20:40:16 +1000

    Update botlog.js

--- commands/botlog.js
+++ commands/botlog.js
@@ -1,32 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const fs = require('fs');
-const readline = require('readline');
 const winston = require('winston');
-const { MessageEmbed } = require('discord.js'); 
-
-const readLastLines = (path, numberOfLines) => {
-  return new Promise((resolve, reject) => {
-    const fileStream = fs.createReadStream(path);
-    const rl = readline.createInterface({
-      input: fileStream,
-      crlfDelay: Infinity
-    });
-
-    let lastLines = [];
-    rl.on('line', (line) => {
-      lastLines.push(line);
-      if (lastLines.length > numberOfLines) {
-        lastLines.shift();
-      }
-    });
-
-    rl.on('close', () => {
-      resolve(lastLines.reverse().join('\n'));
-    });
-
-    rl.on('error', reject);
-  });
-};
+const { MessageEmbed } = require('discord.js');  // Changed from EmbedBuilder to MessageEmbed
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -34,22 +9,22 @@ module.exports = {
         .setDescription('Fetch and display bot logs.'),
     async execute(interaction) {
         await interaction.deferReply();
-
-        try {
-          const data = await readLastLines('./logs/bot.log', 10);
-          if (data.length > 2048) {
-            data = data.substring(0, 2045) + '...';
-          }
-
-          const logEmbed = new MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle('Bot Logs')
-            .setDescription(data);
-          
-          interaction.followUp({ embeds: [logEmbed] });
-        } catch (err) {
-          winston.error(`Error reading log file: ${err}`);
-          interaction.followUp('Error reading log file.');
-        }
+        fs.readFile('./logs/bot.log', 'utf8', (err, data) => {
+            if (err) {
+                winston.error(`Error reading log file: ${err}`);
+                return interaction.followUp('Error reading log file.');
+            }
+            
+            if (data.length > 2048) {
+                data = data.slice(-2045) + '...';  // Getting the last 2045 characters
+            }
+            
+            const logEmbed = new MessageEmbed()  // Changed from EmbedBuilder to MessageEmbed
+                .setColor('#0099ff')
+                .setTitle('Bot Logs')
+                .setDescription(`${data}`);
+                
+            interaction.followUp({ embeds: [logEmbed] });
+        });
     },
 };
