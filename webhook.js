9286a869cf94c59019b7c429d2243113492a166f
matthewhand <matthewhandau@gmail.com>
2023-11-01 14:12:57 +1100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -4,48 +4,47 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBit
 
 client.login(process.env.DISCORD_TOKEN);  // Login to Discord
 
-// Ensure Discord client is ready before starting the webhook server
+const startWebhookServer = (port) => {
+    const app = express();
+    app.use(express.json());
+
+    app.post('/webhook', (req, res) => {
+        // Handle incoming webhook from Replicate
+        console.log('Received webhook:', req.body);
+
+        const predictionResult = req.body;
+        const channelId = process.env.CHANNEL_ID;  // Assuming you have a specific channel to post the result
+        const channel = client.channels.cache.get(channelId);
+
+        if (channel) {
+            const resultMessage = `Prediction Result: ${JSON.stringify(predictionResult, null, 2)}`;
+            channel.send(resultMessage);
+        } else {
+            console.error('Channel not found');
+        }
+
+        res.sendStatus(200);
+    });
+
+    app.get('/health', (req, res) => {
+        // Handle incoming webhook
+        console.debug('Received health probe');
+        res.sendStatus(200);
+    });
+
+    app.get('/uptime', (req, res) => {
+        // Handle incoming webhook
+        console.debug('Received uptime probe');
+        res.sendStatus(200);
+    });
+
+    app.listen(port, () => {
+        console.log(`HTTP server listening at http://localhost:${port}`);
+    });
+};
+
 client.once('ready', () => {
     console.log('Logged in as', client.user.tag);
-
-    const startWebhookServer = (port) => {
-        const app = express();
-        app.use(express.json());
-
-        app.post('/webhook', (req, res) => {
-            // Handle incoming webhook from Replicate
-            console.log('Received webhook:', req.body);
-
-            const predictionResult = req.body;
-            const channelId = process.env.CHANNEL_ID;  // Assuming you have a specific channel to post the result
-            const channel = client.channels.cache.get(channelId);
-
-            if (channel) {
-                const resultMessage = `Prediction Result: ${JSON.stringify(predictionResult, null, 2)}`;
-                channel.send(resultMessage);
-            } else {
-                console.error('Channel not found');
-            }
-
-            res.sendStatus(200);
-        });
-
-        app.get('/health', (req, res) => {
-            // Handle incoming webhook
-            console.debug('Received health probe');
-            res.sendStatus(200);
-        });
-
-        app.get('/uptime', (req, res) => {
-            // Handle incoming webhook
-            console.debug('Received uptime probe');
-            res.sendStatus(200);
-        });
-
-        app.listen(port, () => {
-            console.log(`HTTP server listening at http://localhost:${port}`);
-        });
-    };
 
     // Set the port either from the environment variable or default to 3000
     const port = process.env.PORT || 3000;
@@ -53,5 +52,5 @@ client.once('ready', () => {
 });
 
 module.exports = {
-  startWebhookServer,
+    startWebhookServer,
 };
