0c60c1e76d10d6960028e59d70e40867de9e5061
matthewhand <matthewhandau@gmail.com>
2023-11-02 06:36:46 +1100

    Update utils.js

--- utils.js
+++ utils.js
@@ -2,44 +2,56 @@ const { exec } = require('child_process');
 const fs = require('fs');
 const logger = require('./logger');
 
-const allowedUsers = process.env.ALLOWED_USERS.split(',');
-const isUserAllowed = userId => allowedUsers.includes(userId);
+const allowedUsers = process.env.ALLOWED_USERS ? process.env.ALLOWED_USERS.split(',') : [];
+const allowedRoles = process.env.ALLOWED_ROLES ? process.env.ALLOWED_ROLES.split(',') : [];
+
+const isUserAllowed = (userId, allowedUsersList = allowedUsers) => {
+  return allowedUsersList.includes(userId);
+};
+
+const isRoleAllowed = (userRoles, allowedRolesList = allowedRoles) => {
+  return userRoles.some(role => allowedRolesList.includes(role));
+};
+
 const extractPythonCodeBlocks = content => content.match(/\`\`\`python\n?([\s\S]+?)\`\`\`/g);
-const executePythonCodeBlocks = (message) => {
-    const codeBlocks = extractPythonCodeBlocks(message.content);
-    if (!codeBlocks) {
-        logger.info('No Python code blocks found');
-        return;
-    }
 
-    const userId = message.author.id;
-    if (!isUserAllowed(userId)) {
-        message.reply('You do not have permission to execute this command.');
-        return;
-    }
+const executePythonCodeBlocks = async (message, allowedUsersList = allowedUsers, allowedRolesList = allowedRoles) => {
+  const codeBlocks = extractPythonCodeBlocks(message.content);
+  if (!codeBlocks) {
+    logger.info('No Python code blocks found');
+    return;
+  }
 
-    codeBlocks.forEach((codeBlock) => {
-        const code = codeBlock.replace(/\`\`\`\s*python\s*|\`\`\`/gi, '');
-        executePythonCode(code, message);
-    });
+  const userId = message.author.id;
+  const member = await message.guild.members.fetch(userId);
+  const userRoles = member.roles.cache.map(role => role.id);
+
+  if (!isUserAllowed(userId, allowedUsersList) && !isRoleAllowed(userRoles, allowedRolesList)) {
+    message.reply('You do not have permission to execute this command.');
+    return;
+  }
+
+  codeBlocks.forEach((codeBlock) => {
+    const code = codeBlock.replace(/\`\`\`\s*python\s*|\`\`\`/gi, '');
+    executePythonCode(code, message);
+  });
 };
 
 const executePythonCode = async (code, message) => {
-    const fileName = `tmp_${Date.now()}.py`;
-    fs.writeFileSync(fileName, code);
-    exec(`python ${fileName}`, (error, stdout, stderr) => {
-        if (error) {
-            message.reply(`Error executing code: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            message.reply(`Stderr: ${stderr}`);
-            return;
-        }
-        message.reply(`Stdout: ${stdout}`);
-        fs.unlinkSync(fileName);
-    });
+  const fileName = `tmp_${Date.now()}.py`;
+  fs.writeFileSync(fileName, code);
+  exec(`python ${fileName}`, (error, stdout, stderr) => {
+    if (error) {
+      message.reply(`Error executing code: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      message.reply(`Stderr: ${stderr}`);
+      return;
+    }
+    message.reply(`Stdout: ${stdout}`);
+    fs.unlinkSync(fileName);
+  });
 };
 
-
-module.exports = { extractPythonCodeBlocks, executePythonCode, isUserAllowed, logger };
+module.exports = { extractPythonCodeBlocks, executePythonCode, isUserAllowed, isRoleAllowed, logger };
