a61c242188976af02a3b6d3fc50a5af818ce3557
matthewhand <matthewhandau@gmail.com>
2023-11-01 16:07:33 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const { exec } = require('child_process');
-const { registerCommands, handleCommands } = require('./commands');
-const { startWebhookServer } = require('./webhook');
-const logger = require('./logger');
-const fs = require('fs');
-const { promises: fsPromises } = fs;
-const { DecideToRespond } = require('./responseDecider');
-const Replicate = require('replicate');
-const axios = require('axios');
+import { Client, GatewayIntentBits } from 'discord.js';
+import { registerCommands, handleCommands } from './commands';
+import logger from './logger';
+import { DecideToRespond } from './responseDecider';
+import Replicate from 'replicate';
+import { executePythonCode, extractPythonCodeBlocks, isUserAllowed } from './utilities';
+import { handleImageMessage } from './handleImageMessage';
+import { sendLlmRequest } from './sendLlmRequest';
+import { handleError } from './handleError';
+import { debugEnvVars } from './debugEnvVars';
+import { initializeFetch } from './initializeFetch';
 
 const discordSettings = {
     disableUnsolicitedReplies: false,
@@ -23,29 +24,8 @@ const responseDecider = new DecideToRespond(discordSettings, interrobangBonus, t
 const clientId = process.env.CLIENT_ID;
 const token = process.env.DISCORD_TOKEN;
 const guildId = process.env.GUILD_ID;
-const allowedUsers = process.env.ALLOWED_USERS.split(',');
 const triggerWord = process.env.TRIGGER_WORD || 'pybot';
-const llmUrl = process.env.LLM_URL;
 const llmWakeWords = process.env.LLM_WAKEWORDS ? process.env.LLM_WAKEWORDS.split(',') : [triggerWord];
-
-const isUserAllowed = userId => allowedUsers.includes(userId);
-const extractPythonCodeBlocks = content => content.match(/\`\`\`python\n?([\s\S]+?)\`\`\`/g);
-const executePythonCode = async (code, message) => {
-  const fileName = `tmp_${Date.now()}.py`;
-  fs.writeFileSync(fileName, code);
-  exec(`python ${fileName}`, (error, stdout, stderr) => {
-    if (error) {
-      message.reply(`Error executing code: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      message.reply(`Stderr: ${stderr}`);
-      return;
-    }
-    message.reply(`Stdout: ${stdout}`);
-    fs.unlinkSync(fileName);
-  });
-}
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
@@ -54,201 +34,66 @@ handleCommands(client);
 client.login(token);
 
 logger.info('Bot started successfully.');
-logger.info(`Allowed users: ${allowedUsers}`);
-
 
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
 });
 
-async function handleImageMessage(message, replicate) {
-    try {
-        const attachments = message.attachments;
-        if (attachments.size > 0) {
-            const imageUrl = attachments.first().url;
-
-            console.debug(`Image URL: ${imageUrl}`);  // Debugging line
-
-            // Send a message to the channel
-            await message.channel.send('Image detected. Running analysis using Llava 13b on Replicant...');
-
-            const modelVersion = "2facb4a474a0462c15041b78b1ad70952ea46b5ec6ad29583c0b29dbd4249591";
-            const prediction = await replicate.predictions.create({
-                version: modelVersion,
-                input: {
-                    image: imageUrl,  // Assuming the image is publicly accessible
-                },
-                webhook: process.env.WEBHOOK_URL,  // Use environment variable for webhook URL
-                webhook_events_filter: ["completed"]
-            });
-
-            // Optionally, you could store the prediction ID to correlate the webhook call later
-            const predictionId = prediction.id;
-            console.log(`Prediction ID: ${predictionId}`);  // This line is already providing some debug info
-
-            return true;  // Image was detected and analysis initiated
-        } else {
-            console.debug('No attachments found');  // Debugging line
-            return false;  // No image was detected
-        }
-    } catch (error) {
-        console.error(`Error in handleImageMessage: ${error.message}`);  // Error handling line
-        return false;  // An error occurred, assume no image was detected
-    }
-}
 async function initialize() {
-  const nodeFetch = await import('node-fetch');
-  global.fetch = nodeFetch.default;
-  global.Headers = nodeFetch.Headers;
+  initializeFetch();
 
-  
-client.on('messageCreate', async (message) => {
-  try {
-    if (message.author.id === client.user.id) {
-      return;
-    }
+  client.on('messageCreate', async (message) => {
+    try {
+      if (message.author.id === client.user.id) {
+        return;
+      }
 
-    logger.info(`Received message: ${message.content} from ${message.author.username}`);
+      logger.info(`Received message: ${message.content} from ${message.author.username}`);
 
-    if (message.guild) {
-      const userId = message.author.id;
-      const { shouldReply, isDirectMention } = responseDecider.shouldReplyToMessage(client.user.id, message);
+      if (message.guild) {
+        const userId = message.author.id;
+        const { shouldReply, isDirectMention } = responseDecider.shouldReplyToMessage(client.user.id, message);
 
-      const wakeWordDetected = llmWakeWords.some(wakeWord => 
-        message.content.toLowerCase().includes(wakeWord.toLowerCase())
-      );
+        const wakeWordDetected = llmWakeWords.some(wakeWord => 
+          message.content.toLowerCase().includes(wakeWord.toLowerCase())
+        );
 
-      if (wakeWordDetected || shouldReply || isDirectMention) {
-        logger.info(`wakeWordDetected/shouldReply/isDirectMention in message: ${message.content}`);
+        if (wakeWordDetected || shouldReply || isDirectMention) {
+          logger.info(`wakeWordDetected/shouldReply/isDirectMention in message: ${message.content}`);
 
-        const imageDetected = await handleImageMessage(message, replicate);
-        if (!imageDetected) {
-          const userMessage = message.content;
-
-          const modelToUse = process.env.LLM_MODEL || 'mistral-7b-instruct';  
-
-          // Now create the requestBody object
-          const requestBody = {
-            model: modelToUse,
-            messages: [
-              { role: 'system', content: process.env.LLM_SYSTEM || 'You are a helpful assistant.' },
-              { role: 'user', content: userMessage }
-            ]
-          };        
-          // Prepare headers
-          const headers = {
-              'Content-Type': 'application/json',
-          };
-          if (process.env.LLM_API_KEY) {
-              headers['Authorization'] = `Bearer ${process.env.LLM_API_KEY}`;
+          const imageDetected = await handleImageMessage(message, replicate);
+          if (!imageDetected) {
+            await sendLlmRequest(message);
+          }
+        } else {
+          const codeBlocks = extractPythonCodeBlocks(message.content);
+          if (!codeBlocks) {
+            logger.info('No Python code blocks found');
+            return;
           }
 
-          // Log the request payload if DEBUG is set to true
-          if (process.env.DEBUG === 'true') {
-              console.log('Request payload:', JSON.stringify(requestBody, null, 2));
+          if (!isUserAllowed(userId)) {
+            message.reply('You do not have permission to execute this command.');
+            return;
           }
 
-          // Send a POST request with a JSON body
-          const response = await axios.post(llmUrl, requestBody, { headers: headers });
-
-          if (response.status !== 200) {
-              console.error('Request failed:', response.statusText);
-              // Log the response body if DEBUG is set to true
-              if (process.env.DEBUG === 'true') {
-                  console.error('Response body:', response.data);
-              }
-              // message.reply('Server error.');
-              return;
-          }
-          const responseData = response.data;
-
-          if (responseData && responseData.choices && responseData.choices[0] && responseData.choices[0].message && responseData.choices[0].message.content) {
-              const replyContent = responseData.choices[0].message.content;
-              if (replyContent.length > 2000) {
-                  // Split the message into chunks of 2000 characters or less
-                  const chunks = replyContent.match(/.{1,2000}/g);
-                  for (let chunk of chunks) {
-                      message.reply(chunk);
-                  }
-              } else {
-                  message.reply(replyContent);
-              }
-          } else {
-              message.reply('No response from the server.');
-          }
+          codeBlocks.forEach((codeBlock) => {
+            const code = codeBlock.replace(/\`\`\`\s*python\s*|\`\`\`/gi, '');
+            executePythonCode(code, message);
+          });
         }
       } else {
-        const codeBlocks = extractPythonCodeBlocks(message.content);
-        if (!codeBlocks) {
-          logger.info('No Python code blocks found');
-          return;
+        if (message.content.toLowerCase() === 'ping') {
+          message.reply('Pong!');
         }
-          
-        if (!isUserAllowed(userId)) {
-          message.reply('You do not have permission to execute this command.');
-          return;
-        }
-
-        codeBlocks.forEach((codeBlock) => {
-          const code = codeBlock.replace(/\`\`\`\s*python\s*|\`\`\`/gi, '');
-          executePythonCode(code, message);
-        });
       }
-    } else {
-      if (message.content.toLowerCase() === 'ping') {
-        message.reply('Pong!');
-      }
+    } catch (error) {
+      handleError(error, message);
     }
-  } catch (error) {
-    handleError(error, message);
-  }
-});
-
-
+  });
 }
 
-const handleError = (error, message) => {
-  logger.error(`An error occurred: ${error.message}`);
-  message.channel.send('An error occurred while processing your request.');
-};
-
-function debugEnvVars() {
-  // List of required environment variables
-  const requiredEnvVars = [
-    'CLIENT_ID',
-    'DISCORD_TOKEN',
-    'GUILD_ID',
-    'ALLOWED_USERS',
-    'LLM_URL',
-    'LLM_SYSTEM',
-    'LLM_API_KEY',
-    'PORT'
-  ];
-
-  // Check if DEBUG environment variable is set to true
-  if (process.env.DEBUG && process.env.DEBUG.toLowerCase() === 'true') {
-    // Log all environment variables
-    console.log('Debugging Environment Variables:');
-    requiredEnvVars.forEach(varName => {
-      console.log(`${varName}: ${process.env[varName] || 'Not Set'}`);
-    });
-  }
-
-  // Check if all required environment variables are set
-  const unsetVars = requiredEnvVars.filter(varName => !process.env[varName]);
-  if (unsetVars.length > 0) {
-    console.error(`The following environment variables are not set: ${unsetVars.join(', ')}`);
-    process.exit(1);  // Exit the process with an error code
-  }
-}
-
-// Call the debug function at the start of your application
 debugEnvVars();
-
-//const port = process.env.PORT || 3000;
-//startWebhookServer(port);
-
-
 
 initialize().catch(error => {
   console.error('Error during initialization:', error);
