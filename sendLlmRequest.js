8a6055cc418fa5161f2326b22ca6a06c534a9239
matthewhand <matthewhandau@gmail.com>
2023-11-01 17:10:58 +1100

    Update sendLlmRequest.js

--- sendLlmRequest.js
+++ sendLlmRequest.js
@@ -1,43 +1,51 @@
 const axios = require('axios');
 
 async function sendLlmRequest(message) {
-    const userMessage = message.content;
-    const modelToUse = process.env.LLM_MODEL || 'mistral-7b-instruct';  
-    const requestBody = {
-        model: modelToUse,
-        messages: [
-            { role: 'system', content: process.env.LLM_SYSTEM || 'You are a helpful assistant.' },
-            { role: 'user', content: userMessage }
-        ]
-    };
-    const headers = { 'Content-Type': 'application/json' };
-    if (process.env.LLM_API_KEY) {
-        headers['Authorization'] = `Bearer ${process.env.LLM_API_KEY}`;
-    }
-    if (process.env.DEBUG === 'true') {
-        console.log('Request payload:', JSON.stringify(requestBody, null, 2));
-    }
-    const response = await axios.post(process.env.LLM_URL, requestBody, { headers: headers });
-    if (response.status !== 200) {
-        console.error('Request failed:', response.statusText);
-        if (process.env.DEBUG === 'true') {
-            console.error('Response body:', response.data);
+    try {
+        const userMessage = message.content;
+        const modelToUse = process.env.LLM_MODEL || 'mistral-7b-instruct';  
+        const requestBody = {
+            model: modelToUse,
+            messages: [
+                { role: 'system', content: process.env.LLM_SYSTEM || 'You are a helpful assistant.' },
+                { role: 'user', content: userMessage }
+            ]
+        };
+        const headers = { 'Content-Type': 'application/json' };
+        if (process.env.LLM_API_KEY) {
+            headers['Authorization'] = `Bearer ${process.env.LLM_API_KEY}`;
         }
-        return;
-    }
-    const responseData = response.data;
-    if (responseData && responseData.choices && responseData.choices[0] && responseData.choices[0].message && responseData.choices[0].message.content) {
-        const replyContent = responseData.choices[0].message.content;
-        if (replyContent.length > 2000) {
-            const chunks = replyContent.match(/.{1,2000}/g);
-            for (let chunk of chunks) {
-                message.reply(chunk);
+        if (process.env.DEBUG === 'true') {
+            console.log('Request payload:', JSON.stringify(requestBody, null, 2));
+        }
+        const response = await axios.post(process.env.LLM_URL, requestBody, { headers: headers });
+        if (response.status !== 200) {
+            console.error('Request failed:', response.statusText);
+            if (process.env.DEBUG === 'true') {
+                console.error('Response body:', response.data);
+            }
+            return;
+        }
+        const responseData = response.data;
+        if (responseData && responseData.choices && responseData.choices[0] && responseData.choices[0].message && responseData.choices[0].message.content) {
+            const replyContent = responseData.choices[0].message.content;
+            if (replyContent && replyContent.trim()) {
+                if (replyContent.length > 2000) {
+                    const chunks = replyContent.match(/.{1,2000}/g);
+                    for (let chunk of chunks) {
+                        message.reply(chunk);
+                    }
+                } else {
+                    message.reply(replyContent);
+                }
+            } else {
+                console.error('Empty message not sent.');
             }
         } else {
-            message.reply(replyContent);
+            message.reply('No response from the server.');
         }
-    } else {
-        message.reply('No response from the server.');
+    } catch (error) {
+        console.error('Error in sendLlmRequest:', error.message);
     }
 }
 
