c5afe35957eee3a69a6822812607179ed227fdf0
matthewhand <matthewhandau@gmail.com>
2023-11-01 17:18:04 +1100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,52 +1,69 @@
-const axios = require('axios');
+const express = require('express');
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-async function sendLlmRequest(message) {
-    try {
-        const userMessage = message.content;
-        const modelToUse = process.env.LLM_MODEL || 'mistral-7b-instruct';  
-        const requestBody = {
-            model: modelToUse,
-            messages: [
-                { role: 'system', content: process.env.LLM_SYSTEM || 'You are a helpful assistant.' },
-                { role: 'user', content: userMessage }
-            ]
-        };
-        const headers = { 'Content-Type': 'application/json' };
-        if (process.env.LLM_API_KEY) {
-            headers['Authorization'] = `Bearer ${process.env.LLM_API_KEY}`;
-        }
-        if (process.env.DEBUG === 'true') {
-            console.log('Request payload:', JSON.stringify(requestBody, null, 2));
-        }
-        const response = await axios.post(process.env.LLM_URL, requestBody, { headers: headers });
-        if (response.status !== 200) {
-            console.error('Request failed:', response.statusText);
-            if (process.env.DEBUG === 'true') {
-                console.error('Response body:', response.data);
-            }
-            return;
-        }
-        const responseData = response.data;
-        if (responseData && responseData.choices && responseData.choices[0] && responseData.choices[0].message && responseData.choices[0].message.content) {
-            const replyContent = responseData.choices[0].message.content;
-            if (replyContent && replyContent.trim()) {
-                if (replyContent.length > 2000) {
-                    const chunks = replyContent.match(/.{1,2000}/g);
-                    for (let chunk of chunks) {
-                        message.reply(chunk);
-                    }
-                } else {
-                    message.reply(replyContent);
-                }
-            } else {
-                console.error('Empty message not sent.');
-            }
-        } else {
-            message.reply('No response from the server.');
-        }
-    } catch (error) {
-        console.error('Error in sendLlmRequest:', error.message);
-    }
+if (!process.env.DISCORD_TOKEN || !process.env.CHANNEL_ID) {
+    console.error('Missing required environment variables: DISCORD_TOKEN and/or CHANNEL_ID');
+    process.exit(1);
 }
 
-module.exports = { sendLlmRequest };
+client.login(process.env.DISCORD_TOKEN).catch(error => {
+    console.error('Failed to login to Discord:', error.message);
+    process.exit(1);
+});
+
+const startWebhookServer = (port) => {
+    const app = express();
+    app.use(express.json());
+
+    app.post('/webhook', (req, res) => {
+        console.log('Received webhook:', req.body);
+
+        const predictionResult = req.body;
+        const channelId = process.env.CHANNEL_ID;
+        const channel = client.channels.cache.get(channelId);
+
+        if (channel) {
+            const resultMessage = `Prediction Result: ${JSON.stringify(predictionResult, null, 2)}`;
+            channel.send(resultMessage).catch(error => {
+                console.error('Failed to send message to channel:', error.message);
+            });
+        } else {
+            console.error('Channel not found');
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.sendStatus(200);
+    });
+
+    app.get('/health', (req, res) => {
+        console.debug('Received health probe');
+        res.sendStatus(200);
+    });
+
+    app.get('/uptime', (req, res) => {
+        console.debug('Received uptime probe');
+        res.sendStatus(200);
+    });
+
+    // Error handling middleware
+    app.use((err, req, res, next) => {
+        console.error('Unhandled Error:', err.message);
+        res.status(500).send({ error: 'Server Error' });
+    });
+
+    app.listen(port, () => {
+        console.log(`HTTP server listening at http://localhost:${port}`);
+    });
+};
+
+client.once('ready', () => {
+    console.log('Logged in as', client.user.tag);
+
+    const port = process.env.PORT || 3000;
+    startWebhookServer(port);
+});
+
+module.exports = {
+    startWebhookServer,
+};
