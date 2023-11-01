d8036346afa1ddcb2811617461dcc598ba996fc1
matthewhand <matthewhandau@gmail.com>
2023-11-01 17:39:32 +1100

    Delete discord-settings.js

--- discord-settings.js
+++
@@ -1,8 +0,0 @@
-const discordSettings = {
-    disableUnsolicitedReplies: false,
-    unsolicitedChannelCap: 5,
-    ignore_dms: true,
-    // ... other settings ...
-};
-
-module.exports = discordSettings;
