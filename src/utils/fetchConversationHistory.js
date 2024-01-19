45fc8d37e49876ec01591a3896bd9cdf45373d54
Matthew Hand <matthewhandau@gmail.com>
2024-01-19 21:10:51 +1100

    include username in conv history

--- src/utils/fetchConversationHistory.js
+++ src/utils/fetchConversationHistory.js
@@ -1,10 +1,12 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 
-// Function to fetch conversation history
 async function fetchConversationHistory(channel, limit = 50) {
     try {
         const messages = await channel.messages.fetch({ limit: limit });
-        return messages.map(message => message.content).reverse();
+        return messages.map(message => ({
+            content: message.content,
+            username: message.author.username
+        })).reverse();
     } catch (error) {
         console.error('Error fetching conversation history:', error);
         return [];
