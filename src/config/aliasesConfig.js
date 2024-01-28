67161471f1e94512d2274c31286d5bb274c9d33d
Matthew Hand <matthewhandau@gmail.com>
2024-01-29 06:35:55 +1100

    created aliasesConfig in primordial config/ folder

---
+++ src/config/aliasesConfig.js
@@ -0,0 +1,24 @@
+const aliases = {
+    // HTTP handler
+    'video': 'http:modal',
+
+    // PPLX handler
+    'web': 'perplexity',
+
+    // Flowise handler
+    'gpt4': 'flowise:gpt4',
+    'mtg': 'flowise:qdrant_pplx',
+    'mtg': 'flowise:qdrant_pplx',
+
+    // Quivr handler
+    'deep': 'quivr:philosophic',
+    'geb': 'quivr:philosophic geb',
+    'book': 'quivr:literature',
+    'shakespear': 'quivr:literature shakespear',
+    'suntzu': 'quivr:literature suntzu',
+
+    // Add more aliases her
+
+}
+
+module.exports = { aliases };
--- src/textCommands/commandHandler.js
+++ src/textCommands/commandHandler.js
@@ -6,6 +6,7 @@ const { handlePythonRequest } = require('./handlePythonRequest');
 const { handleHttpCommand } = require('./handleHttpCommand');
 const { handleReportCommand } = require('./handleReportCommand');
 const { handleMuteCommand } = require('./handleMuteCommand');
+const { aliases } = require('../config/aliasesConfig');
 
 const commandHandlers = {
     'image': {
@@ -52,28 +53,6 @@ const commandHandlers = {
         handler: handleHelpCommand, // Defined below
         description: 'Displays this help message. Usage: !help'
     }
-};
-
-const aliases = {
-    // HTTP handler
-    'video': 'http:modal',
-
-    // PPLX handler
-    'web': 'perplexity',
-
-    // Flowise handler
-    'gpt4': 'flowise:gpt4',
-    'mtg': 'flowise:qdrant_pplx',
-    'mtg': 'flowise:qdrant_pplx',
-
-    // Quivr handler
-    'deep': 'quivr:philosophic',
-    'geb': 'quivr:philosophic geb',
-    'book': 'quivr:literature',
-    'shakespear': 'quivr:literature shakespear',
-    'suntzu': 'quivr:literature suntzu',
-
-    // Add more aliases here as needed
 };
 
 function handleAliasCommand(message) {
