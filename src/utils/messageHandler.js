744ceb99b798624b24435db7b461094bedc2d930
Matthew Hand <matthewhandau@gmail.com>
2024-01-31 14:42:51 +1100

    export fix

--- src/utils/messageHandler.js
+++ src/utils/messageHandler.js
@@ -47,66 +47,66 @@ function buildRequestBody(historyMessages, userMessage) {
     return requestBody;
 }
 
-async function sendLlmRequest(message) {
-    try {
-        const historyMessages = await fetchConversationHistory(message.channel);
-        const requestBody = buildRequestBody(historyMessages, message.content);
-        if (!validateRequestBody(requestBody)) throw new Error('Invalid request body');
+// async function sendLlmRequest(message) {
+//     try {
+//         const historyMessages = await fetchConversationHistory(message.channel);
+//         const requestBody = buildRequestBody(historyMessages, message.content);
+//         if (!validateRequestBody(requestBody)) throw new Error('Invalid request body');
 
-        const typingInterval = startTypingIndicator(message.channel);
-        const response = await axios.post(constants.LLM_ENDPOINT_URL, requestBody, {
-            headers: { 'Content-Type': 'application/json', ...(constants.API_KEY && { 'Authorization': `Bearer ${constants.API_KEY}` }) }
-        });
+//         const typingInterval = startTypingIndicator(message.channel);
+//         const response = await axios.post(constants.LLM_ENDPOINT_URL, requestBody, {
+//             headers: { 'Content-Type': 'application/json', ...(constants.API_KEY && { 'Authorization': `Bearer ${constants.API_KEY}` }) }
+//         });
 
-        clearInterval(typingInterval);
-        const replyContent = processResponse(response.data);
-        if (replyContent) {
-            const messagesToSend = splitMessage(replyContent, 2000);
-            for (const msg of messagesToSend) await message.reply(msg);
-            if (constants.FOLLOW_UP_ENABLED) setTimeout(() => sendFollowUpRequest(message), getRandomDelay(constants.FOLLOW_UP_MIN_DELAY, constants.FOLLOW_UP_MAX_DELAY));
-        }
-    } catch (error) {
-        logger.error(`Error in sendLlmRequest: ${error.message}`, error);
-        await message.reply(getRandomErrorMessage());
-    }
-}
+//         clearInterval(typingInterval);
+//         const replyContent = processResponse(response.data);
+//         if (replyContent) {
+//             const messagesToSend = splitMessage(replyContent, 2000);
+//             for (const msg of messagesToSend) await message.reply(msg);
+//             if (constants.FOLLOW_UP_ENABLED) setTimeout(() => sendFollowUpRequest(message), getRandomDelay(constants.FOLLOW_UP_MIN_DELAY, constants.FOLLOW_UP_MAX_DELAY));
+//         }
+//     } catch (error) {
+//         logger.error(`Error in sendLlmRequest: ${error.message}`, error);
+//         await message.reply(getRandomErrorMessage());
+//     }
+// }
 
-async function sendFollowUpRequest(message) {
-    try {
-        const historyMessages = await fetchConversationHistory(message.channel);
-        if (historyMessages.length === 0) return;
+// async function sendFollowUpRequest(message) {
+//     try {
+//         const historyMessages = await fetchConversationHistory(message.channel);
+//         if (historyMessages.length === 0) return;
 
-        const requestBody = buildFollowUpRequestBody(historyMessages);
-        if (!validateRequestBody(requestBody)) throw new Error('Invalid request body for follow-up');
+//         const requestBody = buildFollowUpRequestBody(historyMessages);
+//         if (!validateRequestBody(requestBody)) throw new Error('Invalid request body for follow-up');
 
-        const response = await axios.post(constants.LLM_ENDPOINT_URL, requestBody, {
-            headers: { 'Content-Type': 'application/json', ...(constants.API_KEY && { 'Authorization': `Bearer ${constants.API_KEY}` }) }
-        });
+//         const response = await axios.post(constants.LLM_ENDPOINT_URL, requestBody, {
+//             headers: { 'Content-Type': 'application/json', ...(constants.API_KEY && { 'Authorization': `Bearer ${constants.API_KEY}` }) }
+//         });
 
-        const followUpContent = processResponse(response.data);
-        if (followUpContent) {
-            const messagesToSend = splitMessage(followUpContent, 2000);
-            for (const msg of messagesToSend) await message.reply(msg);
-        }
-    } catch (error) {
-        logger.error(`[Error] Follow-Up Handler: ${error.message}`, error);
-        await message.reply(getRandomErrorMessage());
-    }
-}
+//         const followUpContent = processResponse(response.data);
+//         if (followUpContent) {
+//             const messagesToSend = splitMessage(followUpContent, 2000);
+//             for (const msg of messagesToSend) await message.reply(msg);
+//         }
+//     } catch (error) {
+//         logger.error(`[Error] Follow-Up Handler: ${error.message}`, error);
+//         await message.reply(getRandomErrorMessage());
+//     }
+// }
 
-function buildFollowUpRequestBody(historyMessages) {
-    const randomAlias = aliases[Math.floor(Math.random() * Object.keys(aliases).length)];
-    const command = `!${randomAlias}`;
-    const reflectivePrompt = `Reflecting on the conversation, how might we use the command ${command} for further insights?`;
+// function buildFollowUpRequestBody(historyMessages) {
+//     const randomAlias = aliases[Math.floor(Math.random() * Object.keys(aliases).length)];
+//     const command = `!${randomAlias}`;
+//     const reflectivePrompt = `Reflecting on the conversation, how might we use the command ${command} for further insights?`;
 
-    let requestBody = { model: constants.MODEL_TO_USE, messages: [{ role: 'system', content: reflectivePrompt }] };
-    historyMessages.slice().reverse().forEach(msg => {
-        if (JSON.stringify(requestBody).length <= constants.MAX_CONTENT_LENGTH) {
-            requestBody.messages.push({ role: 'user', content: `<@${msg.userId}>: ${msg.content}` });
-        }
-    });
-    return requestBody;
-}
+//     let requestBody = { model: constants.MODEL_TO_USE, messages: [{ role: 'system', content: reflectivePrompt }] };
+//     historyMessages.slice().reverse().forEach(msg => {
+//         if (JSON.stringify(requestBody).length <= constants.MAX_CONTENT_LENGTH) {
+//             requestBody.messages.push({ role: 'user', content: `<@${msg.userId}>: ${msg.content}` });
+//         }
+//     });
+//     return requestBody;
+// }
 
 async function messageHandler(message) {
     if (message.author.bot) return;
@@ -121,3 +121,5 @@ async function messageHandler(message) {
     // Pass the modified message content to your general command handler
     await commandHandler(message, modifiedMessageContent);
 }
+
+module.exports = { messageHandler };
