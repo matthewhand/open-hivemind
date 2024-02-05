dc15437b8d891479a89beefba517912ceebbcad3
Matthew Hand <matthewhandau@gmail.com>
2024-02-05 17:18:24 +1100

    event handler rego fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+// index.js
 const logger = require('./utils/logger');
 const { registerCommands } = require('./handlers/slashCommandHandler');
 const { debugEnvVars } = require('./config/debugEnvVars');
 const { initializeFetch } = require('./utils/initializeFetch');
 const { startWebhookServer } = require('./handlers/webhookHandler');
 const bot = require('./bot'); // Import the initialized bot from bot.js
+require('./eventhandlers'); // Import the event handlers
 
 // Constants and initialization
 const discordSettings = {
@@ -14,7 +16,7 @@ const discordSettings = {
 
 async function initialize() {
     initializeFetch();
-    const webhookPort = process.env.WEB_SERVER_PORT || 3000;
+    const webhookPort = process.env.WEB SERVER_PORT || 3000;
     startWebhookServer(webhookPort);
 
     registerCommands(bot.client); // Register commands using the bot client
