bf5ae3138bc4f19a9f5004be78228ce193ebfbdc
Matthew Hand <matthewhandau@gmail.com>
2024-02-15 09:40:20 +1100

    improved logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -11,17 +11,32 @@ if (!fs.existsSync(logsDir)) {
 const debugMode = process.env.DEBUG === 'true';
 const logLevel = debugMode ? 'debug' : 'info';
 
+// Define a custom format that includes timestamps and combines multiple formats
+const customFormat = winston.format.combine(
+  winston.format.timestamp({
+    format: 'YYYY-MM-DD HH:mm:ss'
+  }),
+  winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+);
+
 const logger = winston.createLogger({
   level: logLevel,
   format: winston.format.json(),
   transports: [
-    new winston.transports.Console({ format: winston.format.simple() }),
-        // File transport for 'warn' and 'error' logs
-        new winston.transports.File({
-          level: 'warn', // Only logs at 'warn' level and more severe will be logged
-          filename: './logs/bot.log'
-      })
-    ],
+    // Console transport for all logs with timestamp
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(), // Optional, to add color to the console output
+        customFormat
+      )
+    }),
+    // File transport for 'warn' and 'error' logs, including timestamp
+    new winston.transports.File({
+      level: 'warn', // Only logs at 'warn' level and more severe will be logged
+      filename: './logs/bot.log',
+      format: customFormat // Use the custom format for file logging as well
+    })
+  ],
 });
 
 module.exports = logger;
