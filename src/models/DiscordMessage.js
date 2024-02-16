c68a0cc79c42e9487e9551771261b905f97cf18e
Matthew Hand <matthew.hand@datacom.com.au>
2024-02-17 06:18:02 +1100

    isBot fix

--- src/models/DiscordMessage.js
+++ src/models/DiscordMessage.js
@@ -1,9 +1,10 @@
 // src/models/DiscordMessage.js
 const IMessage = require('../interfaces/IMessage');
 const logger = require('../utils/logger');
+const constants = require('../config/constants'); // Ensure this path is correct
 
 class DiscordMessage extends IMessage {
-    constructor(message, isBot = false) { // Added isBot parameter with default value
+    constructor(message, isBot = null) { // isBot parameter reintroduced with a default null value
         super(message);
         
         if (!message) {
@@ -12,15 +13,21 @@ class DiscordMessage extends IMessage {
         }
         
         this.message = message; // Store the original Discord message object
-        this.isBot = isBot; // Use the passed isBot parameter
+        this.isBotExplicitlySet = isBot; // Store the explicitly set isBot value, if any
 
         logger.debug('DiscordMessage constructor: message object successfully initialized.');
     }
 
     isFromBot() {
-        // Use the isBot property set in the constructor
-        logger.debug(`DiscordMessage.isFromBot: Message is from bot: ${this.isBot}`);
-        return this.isBot;
+        // First check if isBot has been explicitly set in the constructor
+        if (this.isBotExplicitlySet !== null) {
+            logger.debug(`DiscordMessage.isFromBot: Using explicitly set value: ${this.isBotExplicitlySet}`);
+            return this.isBotExplicitlySet;
+        }
+        // If not explicitly set, compare the author's ID with CLIENT_ID
+        const isBotMessage = this.message.author && this.message.author.id === constants.CLIENT_ID;
+        logger.debug(`DiscordMessage.isFromBot: Message is from bot based on CLIENT_ID: ${isBotMessage}`);
+        return isBotMessage;
     }
 
     getText() {
