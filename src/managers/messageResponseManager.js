eebd7ac89a38655884ae39fac7bb959fc361f11a
Matthew Hand <matthewhandau@gmail.com>
2024-02-17 12:11:35 +1100

    message response chance debug

--- src/managers/messageResponseManager.js
+++ src/managers/messageResponseManager.js
@@ -58,16 +58,23 @@ class MessageResponseManager {
         return discordMessage.mentionsUsers() || discordMessage.isReply() || channelId === constants.CHANNEL_ID;
     }
 
+
     calculateBaseChance(discordMessage) {
         let baseChance = this.calcBaseChanceOfUnsolicitedReply(discordMessage.getChannelId());
-        baseChance += this.calculateBonusForMessageContent(discordMessage.getText());
+        logger.debug(`Initial base chance based on time since last reply: ${baseChance}`);
+
+        const bonusFromContent = this.calculateBonusForMessageContent(discordMessage.getText());
+        baseChance += bonusFromContent;
+        logger.debug(`Bonus from message content: ${bonusFromContent}, updated base chance: ${baseChance}`);
 
         if (discordMessage.mentionsUsers()) {
             baseChance += this.mentionBonus;
+            logger.debug(`Added mention bonus: ${this.mentionBonus}, updated base chance: ${baseChance}`);
         }
 
         if (discordMessage.isFromBot()) {
             baseChance = Math.max(0, baseChance - this.botResponsePenalty);
+            logger.debug(`Applied bot response penalty: ${this.botResponsePenalty}, updated base chance: ${baseChance}`);
         }
 
         return baseChance;
@@ -75,20 +82,27 @@ class MessageResponseManager {
 
     calcBaseChanceOfUnsolicitedReply(channelId) {
         const timeSinceLastReply = messageResponseUtils.getTimeSinceLastReply(channelId);
-        return this.timeVsResponseChance.reduce((acc, [duration, chance]) => {
+        logger.debug(`Time since last reply for channel ${channelId}: ${timeSinceLastReply}ms`);
+
+        let chance = this.timeVsResponseChance.reduce((acc, [duration, chance]) => {
             if (timeSinceLastReply <= duration) {
+                logger.debug(`Matched duration threshold: ${duration}ms with chance: ${chance}`);
                 return chance;
             }
             return acc;
         }, 0);
+        
+        logger.debug(`Calculated base chance of unsolicited reply based on duration: ${chance}`);
+        return chance;
     }
 
     calculateBonusForMessageContent(messageContent) {
         let bonus = 0;
         if (messageContent.includes('!')) {
             bonus += this.interrobangBonus;
+            logger.debug(`Content contains '!', adding interrobang bonus: ${this.interrobangBonus}`);
         }
-        // Additional content checks can be implemented here
+        // Implement additional content checks here, logging as necessary
         return bonus;
     }
 }
