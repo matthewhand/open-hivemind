ce4c8a0ddc7f927db3dde8066a0b01a6def93d9a
Matthew Hand <matthewhandau@gmail.com>
2024-04-08 09:12:58 +0000

    handleError fix

--- src/utils/handleError.js
+++ src/utils/handleError.js
@@ -1,6 +1,6 @@
 const logger = require('./logger');
 
-function handleError(error, message) {
+function handleError(error) {
     logger.error(`An error occurred: ${error.message}`);
     logger.error(`Error Stack Trace: ${error.stack}`);
     // message.channel.send('An error occurred while processing your request.');
