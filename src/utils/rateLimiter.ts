c335edaab887094c5e9e9ceebb69f1ce56c28798
Matthew Hand <matthewhandau@gmail.com>
2024-08-18 22:59:45 +0000

    Linted and fixed src/utils/rateLimiter.ts

--- src/utils/rateLimiter.ts
+++ src/utils/rateLimiter.ts
@@ -1,4 +1,4 @@
-import { LLM_MESSAGE_LIMIT_PER_HOUR, LLM_MESSAGE_LIMIT_PER_DAY } from '../config/constants';
+import { LLM_MESSAGE_LIMIT_PER_HOUR, LLM_MESSAGE_LIMIT_PER_DAY } from '../config/configurationManager';
 
 /**
  * Rate Limiter for Discord Bot Messages
