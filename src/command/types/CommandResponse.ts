ba795c31120c70106b1b24cbbdffad574f52e41e
Matthew Hand <matthewhandau@gmail.com>
2024-08-19 01:07:13 +0000

    Add CommandResponse type to types directory

---
+++ src/command/types/CommandResponse.ts
@@ -0,0 +1,13 @@
+export class CommandResponse {
+    success: boolean;
+    message: string;
+    data: any;
+    error?: string;
+
+    constructor(success: boolean, message: string, data: any = null, error: string | undefined = undefined) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+        this.error = error;
+    }
+}
