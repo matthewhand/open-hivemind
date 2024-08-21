cffcbd33995bef351be3c802eef9345de7ff9052
Matthew Hand <matthewhandau@gmail.com>
2024-08-21 22:21:16 +0000

    Refactor mute command to correctly access User object and handle tag property.

--- src/command/inline/mute.ts
+++ src/command/inline/mute.ts
@@ -1,6 +1,7 @@
 import { ICommand } from '@src/types/Command';
 import { CommandInteraction } from 'discord.js';
 import { muteUser } from '@command/common/mute';
+import Logger from '@utils/logger';
 
 const muteCommand: ICommand = {
     name: 'mute',
@@ -9,6 +10,8 @@ const muteCommand: ICommand = {
         const target = interaction.options.getMember('target') as GuildMember;
         if (target) {
             await muteUser(interaction, target);
+            Logger.info('User ' + target.user.tag + ' has been muted.');
+            await interaction.reply('User ' + target.user.tag + ' has been muted.');
         } else {
             await interaction.reply('Could not find the specified user.');
         }
