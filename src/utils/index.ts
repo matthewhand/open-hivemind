5f0cb591be50208b6964ba4098daa7402f3bdc2f
Matthew Hand <matthewhandau@gmail.com>
2024-08-22 04:04:26 +0000

    Fixed import paths and addressed syntax issues in TypeScript files

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,13 +3,13 @@
 import * as aliasUtils from './aliasUtils';
 import * as common from './common';
 import { handleError } from '../utils/commonUtils';
-import handleImageMessage from './handleImageMessage';
+import handleImageMessage from "@src/message/helpers/handleImageMessage";
 import logger from './logger';
-import * as mutingUtils from './mutingUtils';
-import parseCommand from './parseCommand';
-import * as permissions from './permissions';
-import reconstructCommandFromAlias from './reconstructCommandFromAlias';
-import sendFollowUpRequest from '@message/sendFollowUpRequest';
+import mutingUtils from "@src/message/helpers/mutingUtils";
+import parseCommand from "@src/message/helpers/parseCommand";
+import permissions from "@src/message/helpers/permissions";
+import { reconstructCommandFromAlias } from "@src/utils/reconstructCommandFromAlias";
+import { sendFollowUpRequest } from "@src/message/sendFollowUpRequest";
 import * as votingUtils from './votingUtils';
 
 export {
