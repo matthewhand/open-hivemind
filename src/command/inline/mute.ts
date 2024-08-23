489633f4ba7c62fcf3ab802a92bbeaa2e5991beb
Matthew Hand <matthewhandau@gmail.com>
2024-08-23 00:16:03 +0000

    Fix type errors by asserting correct types for getUser and getString in mute.ts and ban.ts

--- src/command/inline/mute.ts
+++ src/command/inline/mute.ts
@@ -1,21 +1,23 @@
 import ICommand from '../interfaces/ICommand';
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction } from 'discord.js';
 import { muteUser } from '@src/command/common/mute';
 import logger from '@src/utils/logger';
 
 const muteCommand: ICommand = {
-    name: 'mute',
-    description: 'Mutes a user via inline command.',
-    async execute(interaction: CommandInteraction) {
-        const target = interaction.options.getMember('target') as GuildMember;
-        if (target) {
-            await muteUser(interaction, target);
-            logger.info('User ' + target.user.tag + ' has been muted.');
-            await interaction.reply('User ' + target.user.tag + ' has been muted.');
-        } else {
-            await interaction.reply('Could not find the specified user.');
-        }
-    },
+  name: 'mute',
+  description: 'Mutes a user via inline command.',
+  async execute(interaction: CommandInteraction) {
+    const targetUser = interaction.options.interaction.options.getUser('target') as User || interaction.options.interaction.options.getUser('user') as User;
+    const target = targetUser ? interaction.guild?.members.cache.get(targetUser.id) : undefined;
+
+    if (target) {
+      await muteUser(interaction, target);
+      logger.info('User ' + target.user.tag + ' has been muted.');
+      await interaction.reply('User ' + target.user.tag + ' has been muted.');
+    } else {
+      await interaction.reply('Could not find the specified user.');
+    }
+  },
 };
 
 export default muteCommand;
--- src/command/slash/ban.ts
+++ src/command/slash/ban.ts
@@ -1,9 +1,20 @@
-import { BanCommand } from '@command/common/ban';
 import { CommandInteraction } from 'discord.js';
+import { banUser } from '@src/command/common/ban';
+import ICommand from '../interfaces/ICommand';
 
-export class SlashBanCommand extends BanCommand {
-    async execute(interaction: CommandInteraction): Promise<{ success: boolean, message: string, error?: string }> {
-        const reason = interaction.options.getString('reason') || 'No reason provided';
-        return await this.banUser(interaction, reason);
+const banCommand: ICommand = {
+  name: 'ban',
+  description: 'Bans a user via inline command.',
+  async execute(interaction: CommandInteraction) {
+    const targetUser = interaction.options.interaction.options.getUser('target') as User || interaction.options.getUser('user');
+    const reason = interaction.options.interaction.options.getString('reason') as string || 'No reason provided';
+
+    if (targetUser) {
+      await banUser(interaction, targetUser, reason);
+    } else {
+      await interaction.reply('Could not find the specified user.');
     }
-}
+  },
+};
+
+export default banCommand;
