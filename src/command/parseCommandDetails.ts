2a8d35bead60fcf3563d64665c5cdab4c07a2062
Matthew Hand <matthewhandau@gmail.com>
2024-08-26 20:39:40 +0000

    Fixed debug initialization and improved documentation in parseCommandDetails.ts.

--- src/command/parseCommandDetails.ts
+++ src/command/parseCommandDetails.ts
@@ -1,7 +1,14 @@
-import Debug from "debug";
+import Debug from 'debug';
+
+const debug = Debug('app:parseCommandDetails');
 
 /**
  * Parses the command details from a given input string.
+ * 
+ * This function extracts the command and its arguments from a user input string.
+ * It splits the string by spaces, treating the first word as the command and
+ * the rest as arguments. This parsed data is returned as an object with
+ * `command` and `args` properties.
  * 
  * @param {string} input - The input string containing the command and arguments.
  * @returns {{command: string, args: string[]}} The parsed command and arguments.
