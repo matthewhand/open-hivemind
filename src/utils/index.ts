7a56dac645629f55fa1b22b68ce1c8cd1059001c
Matthew Hand <matthewhandau@gmail.com>
2024-08-27 04:10:21 +0000

    Fix imports in index.ts to align with named and default exports in utility modules.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,41 +1,27 @@
 import Debug from "debug";
-import { aliasUtils } from './aliasUtils';
-import { configUtils } from './configUtils';
-import { encryptionUtils } from './encryptionUtils';
-import { environmentUtils } from './environmentUtils';
-import { getEmoji } from './getEmoji';
-import { getRandomDelay } from './getRandomDelay';
-import { getRandomErrorMessage } from './getRandomErrorMessage';
-import { handleError } from './handleError';
-import { initializeFetch } from './initializeFetch';
-import { logger } from './logger';
-import { processingLocks } from './processingLocks';
-import { reconstructCommandFromAlias } from './reconstructCommandFromAlias';
-import { redactSensitiveInfo } from './redactSensitiveInfo';
-import { sendLlmRequestUtils } from './sendLlmRequestUtils';
-import { splitMessage } from './splitMessage';
-import { startTypingIndicator } from './startTypingIndicator';
-import { utils } from './utils';
+import { getRandomAliasCommand, getAliasDescription, listAllAliases, findAliasesByCategory, getDetailedAliasInfo } from './aliasUtils';
+import { getConfigOrWarn } from './configUtils';
+import { encrypt, decrypt } from './encryptionUtils';
+import { debugEnvVars } from './environmentUtils';
+import logger from './logger';
+import processingLocks from './processingLocks';
+import { executeCommand, readFile } from './utils';
 
 const debug = Debug('app:index');
 
 export {
-    aliasUtils,
-    configUtils,
-    encryptionUtils,
-    environmentUtils,
-    getEmoji,
-    getRandomDelay,
-    getRandomErrorMessage,
-    handleError,
-    initializeFetch,
+    getRandomAliasCommand,
+    getAliasDescription,
+    listAllAliases,
+    findAliasesByCategory,
+    getDetailedAliasInfo,
+    getConfigOrWarn,
+    encrypt,
+    decrypt,
+    debugEnvVars,
     logger,
     processingLocks,
-    reconstructCommandFromAlias,
-    redactSensitiveInfo,
-    sendLlmRequestUtils,
-    splitMessage,
-    startTypingIndicator,
-    utils,
+    executeCommand,
+    readFile,
     debug
 };
