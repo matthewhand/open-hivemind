3e4543035fba625e978ed4cbad1de2bd21f79a16
Matthew Hand <matthewhandau@gmail.com>
2024-08-27 00:59:12 +0000

    Remove redundant prefixes from log statements in votingUtils.

--- src/message/helpers/votingUtils.ts
+++ src/message/helpers/votingUtils.ts
@@ -1,22 +1,33 @@
 import Debug from "debug";
 
+const debug = Debug('app:votingUtils');
+
 /**
  * Simulates the process of starting a voting process for banning a user.
+ *
+ * This function initiates a simulated voting process to decide whether a user should be banned.
+ * It logs the start of the process and returns a hardcoded result for demonstration purposes.
+ *
  * @param userId - The ID of the user to start a ban vote for.
  * @returns A promise resolving with the result of the voting process.
  */
 export async function startVotingProcess(userId: string): Promise<{ votePassed: boolean }> {
-    debug(`[votingUtils] Starting voting process for user ID: \${userId}`);
+    debug(`Starting voting process for user ID: ${userId}`);
     // Simulated voting process result
     return { votePassed: true };
 }
+
 /**
  * Checks if the user is eligible to initiate a voting process this year.
+ *
+ * This function checks if a user is eligible to start a voting process. It logs the eligibility
+ * check and returns a hardcoded boolean value indicating eligibility.
+ *
  * @param userId - The ID of the user to check eligibility for.
  * @returns A boolean indicating if the user is eligible.
  */
 export function checkVotingEligibility(userId: string): boolean {
-    debug(`[votingUtils] Checking voting eligibility for user ID: \${userId}`);
+    debug(`Checking voting eligibility for user ID: ${userId}`);
     // Simulated eligibility check
     return true;
 }
