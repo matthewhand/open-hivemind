2d60bb3b60cd9eec86a6c8b7f191be5bd21c5e9b
Matthew Hand <matthewhandau@gmail.com>
2024-08-27 00:54:41 +0000

    Fix reconstructCommandFromAlias by adding debug initialization and enhancing documentation.

--- src/message/helpers/reconstructCommandFromAlias.ts
+++ src/message/helpers/reconstructCommandFromAlias.ts
@@ -1,5 +1,7 @@
 import Debug from "debug";
 
+const debug = Debug('app:reconstructCommandFromAlias');
+
 export interface Alias {
     command: string;
     description: string;
@@ -9,6 +11,10 @@ export type AliasMapping = Record<string, Alias>;
 
 /**
  * Resolves a command from an alias.
+ *
+ * This function takes an alias provided by the user and looks it up in the alias mapping object.
+ * If the alias is found, it returns the corresponding command; otherwise, it returns the alias itself.
+ *
  * @param alias - The alias provided by the user.
  * @param aliases - The alias mapping object.
  * @returns The resolved command string.
@@ -21,6 +27,10 @@ export function resolveAlias(alias: string, aliases: AliasMapping): string {
 
 /**
  * Reconstructs a full command string from an alias and its arguments.
+ *
+ * This function takes an alias and its arguments, resolves the alias to the full command, and
+ * reconstructs the full command string to be executed.
+ *
  * @param alias - The alias provided by the user.
  * @param args - The arguments passed with the alias.
  * @param aliases - The alias mapping object.
@@ -35,6 +45,10 @@ export function reconstructCommand(alias: string, args: string[], aliases: Alias
 
 /**
  * Retrieves the description for a given alias.
+ *
+ * This function looks up the provided alias in the alias mapping object and returns its description.
+ * If the alias is not found, it returns a default message indicating that no description is available.
+ *
  * @param alias - The alias to look up.
  * @param aliases - The alias mapping object.
  * @returns The description of the alias.
@@ -47,6 +61,9 @@ export function getAliasDescription(alias: string, aliases: AliasMapping): strin
 
 /**
  * Lists all available aliases.
+ *
+ * This function returns an array of all the alias names defined in the alias mapping object.
+ *
  * @param aliases - The alias mapping object.
  * @returns An array of alias names.
  */
@@ -55,4 +72,3 @@ export function listAliases(aliases: AliasMapping): string[] {
     debug(`listAliases: Available aliases - ${aliasList.join(', ')}`);
     return aliasList;
 }
-
