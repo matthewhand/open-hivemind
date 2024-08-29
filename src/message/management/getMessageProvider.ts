a28cf1cf4ac531b97fd2f500ffc618028f6f9082
Matthew Hand <matthewhandau@gmail.com>
2024-08-29 20:30:44 +0000

    Fixed discordConfig reference in getMessageProvider.ts to use getConfig("discord").

--- src/message/management/getMessageProvider.ts
+++ src/message/management/getMessageProvider.ts
@@ -16,7 +16,7 @@ const debug = Debug('app:getMessageProvider');
  */
 export function getMessageProvider() {
   const configManager = ConfigurationManager.getInstance(); // Instantiate ConfigurationManager
-  const messageProvider = configManager.discordConfig.MESSAGE_PROVIDER;
+  const messageProvider = configManager.getConfig("discord").MESSAGE_PROVIDER;
 
   debug('Configured message provider:', messageProvider);
 
