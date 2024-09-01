d75d976ddf14ac38aa139f20bec1e3491e10c426
Matthew Hand <matthewhandau@gmail.com>
2024-09-01 00:31:00 +0000

    Updated PythonConfig.ts with safe config handling using config.has checks.

--- src/command/config/pythonConfig.ts
+++ src/command/config/pythonConfig.ts
@@ -1,6 +1,10 @@
+import config from 'config';
+
 export default class PythonConfig {
-    public readonly PYTHON_EXEC_PATH: string = '/usr/bin/python3';
+    public readonly PYTHON_EXEC_PATH: string;
+
     constructor() {
+        this.PYTHON_EXEC_PATH = process.env.PYTHON_EXEC_PATH || (config.has('python.PYTHON_EXEC_PATH') ? config.get<string>('python.PYTHON_EXEC_PATH') : '/usr/bin/python3');
         console.log('PythonConfig initialized');
     }
 }
