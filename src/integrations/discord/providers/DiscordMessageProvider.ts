fde5cbbd3a3a711efc20cb80a17f6e4f6c34b363
Matthew Hand <matthewhandau@gmail.com>
2025-02-22 09:03:03 +0000

    refactor(discord): remove unused code in DiscordMessageProvider.ts

--- src/integrations/discord/providers/DiscordMessageProvider.ts
+++ src/integrations/discord/providers/DiscordMessageProvider.ts
@@ -1,28 +1,29 @@
-const DiscordSvcLib = require('../DiscordService');
-
 class DiscordMessageProviderImpl {
   discordService: any;
-
+  discordSvcLib: any;
+  
   constructor() {
-    this.discordService = DiscordSvcLib.Discord.DiscordService.getInstance();
+    this.discordSvcLib = require('../DiscordService');
+    this.discordService = this.discordSvcLib.Discord.DiscordService.getInstance();
   }
-
+  
   async sendMessage(channelId: any, message: any, senderName: any) {
     return await this.discordService.sendMessageToChannel(channelId, message, senderName);
   }
-
+  
   async getMessages(channelId: any) {
     const messages = await this.discordService.fetchMessages(channelId);
-    return messages.map((msg: any) => new DiscordSvcLib.DiscordMessage(msg));
+    return messages.map((msg: any) => new this.discordSvcLib.DiscordMessage(msg));
   }
-
+  
   async sendMessageToChannel(channelId: any, message: any, active_agent_name: any) {
     return await this.discordService.sendMessageToChannel(channelId, message, active_agent_name);
   }
-
+  
   getClientId() {
     return this.discordService.getClientId();
   }
 }
-
-module.exports = { DiscordMessageProvider: DiscordMessageProviderImpl };
+ 
+module.exports = DiscordMessageProviderImpl;
+module.exports.DiscordMessageProvider = DiscordMessageProviderImpl;
