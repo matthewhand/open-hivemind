041320c22d827745b594f8e6eb049d4ad4b445b5
Matthew Hand <matthewhandau@gmail.com>
2025-02-22 03:53:18 +0000

    feat: add shouldProcessMessage.test.ts for decay logic testing

---
+++ tests/message/helpers/processing/shouldProcessMessage.test.ts
@@ -0,0 +1,22 @@
+jest.mock('@message/interfaces/messageConfig', () => ({
+  get: jest.fn()
+}));
+
+const shouldProcessModule = require('@message/helpers/processing/shouldProcessMessage');
+const msgConfigMock = require('@message/interfaces/messageConfig');
+
+describe('getMinIntervalMs', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return MESSAGE_MIN_INTERVAL_MS when set', () => {
+    msgConfigMock.get.mockReturnValue(2000);
+    expect(shouldProcessModule.getMinIntervalMs()).toBe(2000);
+  });
+
+  it('should return 1000 as default when MESSAGE_MIN_INTERVAL_MS is not set', () => {
+    msgConfigMock.get.mockReturnValue(undefined);
+    expect(shouldProcessModule.getMinIntervalMs()).toBe(1000);
+  });
+});
