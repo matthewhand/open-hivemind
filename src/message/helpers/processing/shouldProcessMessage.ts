f88d083677c6848361095bc05aebaeed3e38de43
Matthew Hand <matthewhandau@gmail.com>
2025-02-22 22:59:37 +0000

    refactor: update shouldProcessMessage to ES module with proper types

--- src/message/helpers/processing/shouldProcessMessage.ts
+++ src/message/helpers/processing/shouldProcessMessage.ts
@@ -1,8 +1,10 @@
-const shouldDebug = require('debug')('app:shouldProcessMessage');
-const msgConfig = require('@message/interfaces/messageConfig');
+import messageConfig from '@config/messageConfig';
 
-function getMinIntervalMs() {
-  return Number(msgConfig.get('MESSAGE_MIN_INTERVAL_MS') || 1000);
+/**
+ * Returns the minimum interval in milliseconds for processing messages.
+ * If the configuration key MESSAGE_MIN_INTERVAL_MS is not set, defaults to 1000.
+ */
+export function getMinIntervalMs(): number {
+  const value = messageConfig.get('MESSAGE_MIN_INTERVAL_MS');
+  return typeof value === 'number' ? value : Number(value) || 1000;
 }
-
-module.exports = { getMinIntervalMs };
