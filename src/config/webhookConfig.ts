87c35856f8d2cb0071769d25ebfbbef829ac2d9b
Matthew Hand <matthewhandau@gmail.com>
2025-02-24 10:04:27 +0000

    config: update webhook configuration

--- src/config/webhookConfig.ts
+++ src/config/webhookConfig.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 const webhookConfig = convict({
   WEBHOOK_ENABLED: {
-    doc: 'Enable webhook functionality',
+    doc: 'Whether to enable the webhook service',
     format: Boolean,
     default: false,
     env: 'WEBHOOK_ENABLED'
@@ -13,6 +13,24 @@ const webhookConfig = convict({
     format: String,
     default: '',
     env: 'WEBHOOK_URL'
+  },
+  WEBHOOK_TOKEN: {
+    doc: 'Token used to verify incoming webhook requests',
+    format: String,
+    default: '',
+    env: 'WEBHOOK_TOKEN'
+  },
+  WEBHOOK_IP_WHITELIST: {
+    doc: 'Comma-separated list of IPs allowed to send webhook requests',
+    format: String,
+    default: '',
+    env: 'WEBHOOK_IP_WHITELIST'
+  },
+  WEBHOOK_PORT: {
+    doc: 'The port to run the webhook on',
+    format: 'port',
+    default: 80,
+    env: 'WEBHOOK_PORT'
   }
 });
 
