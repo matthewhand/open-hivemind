ffacde0f2b22ef7c8d8cfdfecf283ee432e028a6
Matthew Hand <mhand5@une.edu.au>
2026-03-08 02:56:59 +0000

    fix(slack): cast data.channel to string in getChannelId()

--- packages/adapter-slack/src/SlackMessage.ts
+++ packages/adapter-slack/src/SlackMessage.ts
@@ -70,7 +70,7 @@ export default class SlackMessage extends IMessage {
   }
 
   getChannelId(): string {
-    return this.channelId || this.data?.channel || '';
+    return this.channelId || String(this.data?.channel ?? '') || '';
   }
 
   getAuthorId(): string {
