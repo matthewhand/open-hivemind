1462858c3267ed3c638d3ddee3335418648bb794
Matthew Hand <mhand5@une.edu.au>
2026-03-12 08:12:06 +0000

    widen: export getDefaultChannelId() for testability and independent mocking

--- src/message/helpers/processing/shouldProcessMessage.ts
+++ src/message/helpers/processing/shouldProcessMessage.ts
@@ -22,7 +22,7 @@ export function getMinIntervalMs(): number {
 /**
  * Gets the default channel ID from the appropriate config based on provider.
  */
-function getDefaultChannelId(): string {
+export function getDefaultChannelId(): string {
   try {
     // Try Discord config first
     const discordConfig = require('@config/discordConfig').default;
