0e859a33de8171bc7e3a3d8c9a01309e25e8db15
Matthew Hand <mhand5@une.edu.au>
2026-03-21 23:44:12 +0000

    chore: remove agent artifacts

--- run-tests.js
+++
@@ -1,2 +0,0 @@
-// We already verified playwright tests had a pre-existing blocker.
-console.log("Skipping UI Playwright tests due to existing Objects are not valid as React child render blocker as verified earlier.");
