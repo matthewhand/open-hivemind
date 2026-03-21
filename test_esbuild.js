b4b37b06aba7617d1b63cc0498921738df704f39
Matthew Hand <mhand5@une.edu.au>
2026-03-21 21:37:32 +0000

    chore: remove agent artifacts

--- test_esbuild.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  execSync('npx esbuild src/netlify/functions/server.ts --bundle --platform=node --target=node20 --outfile=dist/netlify/functions/server.js --external:express --external:cors --external:serverless-http --external:sqlite3 --external:better-sqlite3 --external:mock-aws-s3 --external:nock --external:aws-sdk');
-  console.error("SUCCESS!");
-} catch (e) {
-  console.error("FAIL", e);
-}
--- test_netlify_toml.py
+++
@@ -1,7 +0,0 @@
-with open("netlify.toml") as f:
-  c = f.read()
-
-if "node_bundler" not in c:
-  with open("netlify.toml", "w") as f:
-    c = c.replace("[[redirects]]", "[functions]\n  node_bundler = \"esbuild\"\n\n[[redirects]]", 1)
-    f.write(c)
