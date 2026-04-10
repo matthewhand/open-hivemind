61ee0c1b98a72532e835d4ba62d4d5fd96927d02
mhand <matthewhandau@gmail.com>
2026-04-11 07:23:41 +1000

    fix(ui): announcements as carousel item linking to GitHub (#2518)

--- src/client/src/components/WelcomeSplash.tsx
+++ src/client/src/components/WelcomeSplash.tsx
@@ -16,7 +16,7 @@ import { Alert } from './DaisyUI/Alert';
 import { Badge } from './DaisyUI/Badge';
 import Steps from './DaisyUI/Steps';
 import { apiService } from '../services/api';
-import AnnouncementBanner from './AnnouncementBanner';
+import TipRotator from './TipRotator';
 
 // ---------------------------------------------------------------------------
 // Types
@@ -167,8 +167,8 @@ const WelcomeSplash: React.FC = () => {
         </p>
       </div>
 
-      {/* Announcement Banner */}
-      <AnnouncementBanner />
+      {/* User Tips */}
+      <TipRotator className="flex items-center gap-2 text-sm text-base-content/70 px-4" />
 
       {/* Configuration Progress */}
       <Card className="bg-base-100 shadow-lg">
